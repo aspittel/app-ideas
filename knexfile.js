f0bf1fb7c5d1e262d1e590961cc85b7eadb4ab34
aspittel <ali07cat07@gmail.com>
2018-02-19 16:36:45 -0500

    remove console.log

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const pg = require('pg')
 pg.defaults.ssl = true
 
-console.log(process.env.DATABASE_URL)
 module.exports = {
   client: 'pg',
   connection: process.env.DATABASE_URL
